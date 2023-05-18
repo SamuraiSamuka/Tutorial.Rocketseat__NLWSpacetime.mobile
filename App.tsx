2236ca5f3796d1b292264cef972aba0e8860a000
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-05-17 21:51:41 -0300

    Adicionando fontes e cores

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="font-alt text-5xl font-bold text-gray-50">
+        Rocketseat
+      </Text>
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
